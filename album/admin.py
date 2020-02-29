04640fb68b7e28fba6d7d64ef1fe163a21f74106
JamesOdera <nyangoma.odera@gmail.com>
2020-02-29 19:59:35 +0300

    make the site url the development server

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Editor,Article,tags,Category,Location
 
 # Register your models here.
+
+admin.site.register(Editor)
+admin.site.register(Article)
+admin.site.register(tags)
+admin.site.register(Category)
+admin.site.register(Location)
